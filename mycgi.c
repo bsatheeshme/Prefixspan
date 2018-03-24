34b1e7b2c9b031e71fcfdcc483ef243b6db86815
Satheesh <bsatheeshme@gmail.com>
2018-03-25 00:32:04 +0530

    s

--- mycgi.c
+++ mycgi.c
@@ -2,7 +2,7 @@
 int main(void) {
   printf("Content-Type: text/plain;charset=us-ascii\n\n");
   printf("Hello world\n\n");
-  printf("Program\n");
+  printf("Prgram\n");
   printf("End of Program\n");
   return 0;
 }
