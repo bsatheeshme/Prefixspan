41f32c05f5acd638de975b53f086ab2c3083fdea
Satheesh <bsatheeshme@gmail.com>
2018-03-24 23:10:11 +0530

    Added new line character.

--- mycgi.c
+++ mycgi.c
@@ -2,6 +2,6 @@
 int main(void) {
   printf("Content-Type: text/plain;charset=us-ascii\n\n");
   printf("Hello world\n\n");
-  printf("End of Program");
+  printf("End of Program\n");
   return 0;
 }
